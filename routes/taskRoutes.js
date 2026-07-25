e3d616e8c11d54c5a008ab243f30eb3ee598efc2
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-07-25 10:12:47 -0700

    Add Assignment 4 task routes and authentication setup

---
+++ routes/taskRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const taskController = require("../controllers/taskController");
+
+router.post("/", taskController.create);
+router.get("/", taskController.index);
+router.get("/:id", taskController.show);
+router.patch("/:id", taskController.update);
+router.delete("/:id", taskController.deleteTask);
+
+module.exports = router;
